e24288e818b4aa7d6492c98f1f9c4a2e8a896f5c
pcsmomo <dongwoo_333@hanmail.net>
2021-08-12 20:37:43 +1000

    17. [NEW] Fetching Data With JavaScript

---
+++ 01-quote-generator/script-local.js
@@ -0,0 +1,8 @@
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  console.log(quote);
+}
+
+newQuote();
