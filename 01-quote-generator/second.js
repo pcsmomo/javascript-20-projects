b2fa5ea7fb16b2093c6e02195ba26b602b0318ae
pcsmomo <dongwoo_333@hanmail.net>
2021-08-14 21:52:53 +1000

    25. [OPTIONAL] Manipulating DOM With JavaScript

--- 01-quote-generator/second.js
+++ 01-quote-generator/second.js
@@ -1,3 +1,10 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 // Get Quote From API
 async function getQuote() {
   // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -8,11 +15,35 @@ async function getQuote() {
     // const response = await fetch(proxyUrl + apiUrl);
     const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data);
+
+    // If Author is blank, add 'Unknown'
+    if (!data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 50) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     console.log('whopps, no quote', error);
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
