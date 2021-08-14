f28d7a9b2c000051d5a9716e0e5102202c332a02
pcsmomo <dongwoo_333@hanmail.net>
2021-08-14 21:56:26 +1000

    26. [OPTIONAL] Creating A Loader With JavaScript

--- 01-quote-generator/second.js
+++ 01-quote-generator/second.js
@@ -5,8 +5,24 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
 // Get Quote From API
 async function getQuote() {
+  loading();
+
   // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
     'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -30,7 +46,9 @@ async function getQuote() {
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerText = data.quoteText;
+    complete();
   } catch (error) {
+    getQuote(); // this API has an error
     console.log('whopps, no quote', error);
   }
 }
