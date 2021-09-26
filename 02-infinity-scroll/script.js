e197a34411367f3e9d392f51582097b1804f0388
pcsmomo <dongwoo_333@hanmail.net>
2021-09-27 08:41:05 +1000

    35. Infinite Scroll With JavaScript

--- 02-infinity-scroll/script.js
+++ 02-infinity-scroll/script.js
@@ -56,5 +56,16 @@ async function getPhotos() {
   }
 }
 
+// Check to see if scrolling near bottom of page, Load more Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >=
+    document.body.offsetHeight - 1000
+  ) {
+    getPhotos();
+    console.log("load more");
+  }
+});
+
 // On Load
 getPhotos();
