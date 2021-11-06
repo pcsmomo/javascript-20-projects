9e43ae18e7583f886ab0044c209cf729a4da3496
pcsmomo <dongwoo_333@hanmail.net>
2021-11-06 16:02:07 +1100

    146. JS - Favorites DOM Elements

--- 14-NASA-APOD/script.js
+++ 14-NASA-APOD/script.js
@@ -47,8 +47,11 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 // let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-  resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+  const currentArray =
+    page === "results" ? resultsArray : Object.values(favorites);
+  console.log("Current Array", page, currentArray);
+  currentArray.forEach((result) => {
     // Card Container
     const card = document.createElement("div");
     card.classList.add("card");
@@ -100,13 +103,21 @@ function updateDOM() {
   });
 }
 
+function updateDOM(page) {
+  // Get Favorites from localStorage
+  if (localStorage.getItem("nasaFavorites")) {
+    favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
+    console.log(favorites);
+  }
+  createDOMNodes(page);
+}
+
 // Get 10 Images from NASA API
 async function getNasaPictures() {
   try {
     // const response = await fetch(apiUrl);
     // const resultsArray = await response.json();
-    console.log(resultsArray);
-    updateDOM();
+    updateDOM("favorites");
   } catch (errir) {
     // Catch Error Here
   }
